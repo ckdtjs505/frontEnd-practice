cb4bfdde1cf47326323c37ec3a3e5278834faaf6
ckdtjs505 <ckdtjs505@naver.com>
2020-06-04 21:29:15 +0900

    Feat: url 3개 제한 구현

--- url-shortening-api-master/assets/main.js
+++ url-shortening-api-master/assets/main.js
@@ -1,13 +1,13 @@
 const div = document.createElement("div");
 
 const inputBox = document.querySelector(".input-box input");
+const outputBox = document.querySelector(".output-box");
 const button = document.querySelector(".input-box button");
 
 let urlList = [];
 
 const createShortURLDom = url => {
   console.log(url);
-  const outputBox = document.querySelector(".output-box");
   const urlLink = "https://rel.ink/";
   const containerDom = document.createElement("div");
   const html = `
@@ -21,7 +21,7 @@ const createShortURLDom = url => {
     </div>
   </div>`;
   containerDom.innerHTML = html;
-  outputBox.appendChild(containerDom);
+  outputBox.prepend(containerDom);
 };
 
 const getShortlyUrl = url => {
@@ -33,16 +33,29 @@ const getShortlyUrl = url => {
 };
 
 button.addEventListener("click", () => {
+  if (inputBox.value === "") return alert("값을 입력하세요");
+
   getShortlyUrl(inputBox.value).then(data => {
-    if (urlList.length >= 3) urlList.pop();
-    urlList.push(data);
-    localStorage.setItem("localUrl", JSON.stringify(urlList));
     createShortURLDom(data);
+
+    // 데이터의 길이가 3개부터는 한개를 지운다
+    if (urlList.length === 3) {
+      urlList.shift();
+      outputBox.lastChild.remove();
+    }
+
+    // 받은데이터를 할당한다.
+    urlList.push(data);
+
+    // 할당한 데이터를 브라우저에 저장한다.
+    localStorage.setItem("localUrl", JSON.stringify(urlList));
   });
 });
 
+// localStorage에는 string으로 저장되어 json parse가 필요
 let currentUrlData = JSON.parse(localStorage.getItem("localUrl"));
-if (currentUrlData !== null || currentUrlData.lenght < 3) {
+
+if (currentUrlData !== null || currentUrlData.lenght <= 3) {
   currentUrlData.forEach(ele => {
     urlList.push(ele);
     createShortURLDom(ele);
