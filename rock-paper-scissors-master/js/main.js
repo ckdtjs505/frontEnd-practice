aa3c0c2d212eaa0a005696b49175aba79011d0c5
ckdtjs505 <ckdtjs505@naver.com>
2020-06-15 22:10:10 +0900

    Feat: step two create

--- rock-paper-scissors-master/js/main.js
+++ rock-paper-scissors-master/js/main.js
@@ -1,6 +1,7 @@
 const stepOne = document.querySelector(".content-step-one");
 const stepTwo = document.querySelector(".content-step-two");
 let stepOneDom = document.createElement("div");
+let stepTwoDom = document.createElement("div");
 
 const createStepOneUI = () => {
   // UI 생성
@@ -9,7 +10,7 @@ const createStepOneUI = () => {
       <div class="paper">
       <img src="./images/icon-paper.svg" alt="">
       </div>
-      <div class="scisors">
+      <div class="scissors">
       <img src="./images/icon-scissors.svg" alt="">
       </div>
       <div class="rock">
@@ -24,11 +25,39 @@ const createStepOneUI = () => {
   score.innerHTML = "0";
 };
 
+const createStepTwoUI = value => {
+  stepTwoDom.classList.add("content-step-two");
+  stepTwoDom.innerHTML = `
+  <div class="user-pick">
+    YOU PICKED
+    <div class="user-value ${value}">
+      <img src="./images/icon-${value}.svg" alt="" />
+    </div>
+  </div>
+
+  <div class="content-step-three">
+    YOU LOSE
+    <button>PLAY AGAIN</button>
+  </div>
+
+  <div class="computer-pick">
+    THE HOUSE PICKED
+    <div class="computer-value none">
+      <img src="./images/icon-scissors.svg" alt="" />
+    </div>
+  </div>
+  `;
+
+  document.body.appendChild(stepTwoDom);
+};
+
 const eventBindStepOne = () => {
   let userButton = stepOneDom.querySelectorAll("div");
   userButton.forEach(e => {
     e.addEventListener("click", function () {
-      console.log(this);
+      console.log(this.className);
+      stepOneDom.remove();
+      createStepTwoUI(this.className);
     });
   });
 };
