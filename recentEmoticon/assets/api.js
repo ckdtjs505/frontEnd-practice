ce8f990ebb5e3610b11016dfe5f323a1e1a8432f
ckdtjs505 <ckdtjs505@naver.com>
2020-08-12 21:37:49 +0900

    Feat: 최근사용 이모티콘 가져오기

--- recentEmoticon/assets/api.js
+++ recentEmoticon/assets/api.js
@@ -7,5 +7,3 @@ class emoticonApi {
     }
 }
 
-
-export 
--- recentEmoticon/assets/main.js
+++ recentEmoticon/assets/main.js
@@ -14,8 +14,15 @@ class Emoticon {
         fetch(`https://emoji-api.com/categories/travel-places?access_key=b44e8c37cf2639a8c1382bad72b6ab8ef837ea75`)
         .then( response => response.json())
         .then( data => { 
-            this.emoticonList =  data.map( ele => ele.character);
-            document.querySelector('.list ul').innerHTML = this.emoticonList;
+            this.emoticonList =  data.map( ele => `
+                <span> 
+                    <a href="javascript:;">
+                        ${ele.character}
+                    </a>
+                </span>`
+            )
+                
+            document.querySelector('.list ').innerHTML = this.emoticonList.join('');
         })
     }
 }
