506f2128e23d0a3e4d312ee9aa918fb87f4df681
ckdtjs505 <ckdtjs505@naver.com>
2020-08-13 21:41:55 +0900

    Feat: event 추가

--- recentEmoticon/assets/main.js
+++ recentEmoticon/assets/main.js
@@ -5,13 +5,23 @@ class Emoticon {
         this.emoticonList;
         this.setEmoticonData();
         this.buildUI();
+        this.event();
     }
 
     buildUI(){
     }
 
+    event(){
+        document.querySelectorAll('.list').forEach( ele => {
+            ele.addEventListener("click", function(e){
+                if( e.target === this)  return;
+                document.querySelector('input').value += e.target.innerText;
+            })
+        })
+    }
+
     setEmoticonData(){
-        fetch(`https://emoji-api.com/categories/travel-places?access_key=b44e8c37cf2639a8c1382bad72b6ab8ef837ea75`)
+        fetch(`https://emoji-api.com/categories/smileys-emotion?access_key=b44e8c37cf2639a8c1382bad72b6ab8ef837ea75`)
         .then( response => response.json())
         .then( data => { 
             this.emoticonList =  data.map( ele => `
