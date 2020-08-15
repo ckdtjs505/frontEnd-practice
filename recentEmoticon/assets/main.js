75b5e505610e530b4ab00896c67b1b692a1bac3c
ckdtjs505 <ckdtjs505@naver.com>
2020-08-15 22:30:12 +0900

    Feat: 이모티콘 탭 이벤트 추가

--- recentEmoticon/assets/main.js
+++ recentEmoticon/assets/main.js
@@ -3,25 +3,38 @@ class Emoticon {
     constructor(){
 
         this.emoticonList;
-        this.setEmoticonData();
+       
         this.buildUI();
         this.event();
     }
 
     buildUI(){
+        this.emoticonContainer = document.querySelector('.emoticonContainer');
+        this.onEmoticonTap = this.emoticonContainer.querySelector('.tap .on');
+        this.emoticonList = this.emoticonContainer.querySelector('.emoticonList');
+        
+        this.setEmoticonData( this.onEmoticonTap.getAttribute('data-type') );
     }
 
     event(){
-        document.querySelectorAll('.list').forEach( ele => {
+        // 이모티콘 리스트 이벤트 바인딩 
+        this.emoticonList.querySelectorAll('.list').forEach( ele => {
             ele.addEventListener("click", function(e){
                 if( e.target === this)  return;
                 document.querySelector('input').value += e.target.innerText;
             })
         })
+        
+        // 이모티콘 탭 이벤트 바인딩
+        this.emoticonContainer.querySelector('.tap').addEventListener("click",(e) => {
+            if( e.target === this)  return;
+            this.setEmoticonData( e.target.getAttribute('data-type') );
+        });
+ 
     }
 
-    setEmoticonData(){
-        fetch(`https://emoji-api.com/categories/smileys-emotion?access_key=b44e8c37cf2639a8c1382bad72b6ab8ef837ea75`)
+    setEmoticonData( list ){
+        fetch(`https://emoji-api.com/categories/${list}?access_key=b44e8c37cf2639a8c1382bad72b6ab8ef837ea75`)
         .then( response => response.json())
         .then( data => { 
             this.emoticonList =  data.map( ele => `
