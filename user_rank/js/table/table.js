0410b37f7489d26b29d574124e2850414d981d84
소니아_오창선 <ckdtjs505@afreecatv.com>
2023-11-14 08:00:34 +0900

    feat : cdn file local 저장

--- user_rank/js/table/table.js
+++ user_rank/js/table/table.js
@@ -10,267 +10,116 @@ class Table {
 	}
 }
 
-class BalloonTable extends Table {
+class SupportTable extends Table {
+	constructor() {
+		super();
+		this.gridOptions = {
+			// each entry here represents one column
+			columnDefs: [
+				{
+					lockPosition: 'left',
+					valueGetter: 'node.rowIndex+1',
+					cellClass: 'locked-col',
+					minWidth: 40,
+					maxWidth: 40,
+					suppressNavigable: true,
+					sortable: false,
+				},
+				{
+					field: 'broadcastStation',
+					headerName: '',
+					maxWidth: 49,
+					sortable: false,
+					cellRenderer: ({ data: { userId } }) => {
+						return `<img style="width:25px; height:25px; border-radius: 50%;" src="//profile.img.afreecatv.com/LOGO/${userId.substring(
+							0,
+							2
+						)}/${userId}/${userId}.jpg" onclick="window.open('https://bj.afreecatv.com/${userId}')" alt="">`;
+					},
+				},
+				{ field: 'userNickname', headerName: '닉네임', maxWidth: 100 },
+				{ field: 'count', headerName: '선물개수', maxWidth: 90 },
+				{ field: 'sendCount', headerName: '선물횟수', maxWidth: 90 },
+				{
+					field: 'sendPercent',
+					headerName: '선물개수비율',
+					cellRenderer: 'agSparklineCellRenderer',
+					cellRendererParams: {
+						sparklineOptions: {
+							type: 'bar',
+							fill: '#5470c6',
+							stroke: '#91cc75',
+							highlightStyle: {
+								fill: '#fac858',
+							},
+							valueAxisDomain: [0, 1],
+							paddingOuter: 0,
+							padding: {
+								top: 0,
+								bottom: 0,
+							},
+							axis: {
+								strokeWidth: 0,
+							},
+						},
+					},
+					maxWidth: 115,
+				},
+			],
+
+			// default col def properties get applied to all columns
+			defaultColDef: { sortable: true, resizable: true },
+			paginationPageSize: 10,
+			animateRows: true, // have rows animate to new positions when sorted
+
+			noRowsOverlayComponent: class {
+				init(params) {
+					this.eGui = document.createElement('div');
+					this.eGui.innerHTML = `<div>
+							<div>  ${params.noRowsMessageFunc()} </div>
+						</div>`;
+				}
+				
+				getGui() {
+					return this.eGui;
+				}
+			
+				refresh(params) {
+					return false;
+				}
+			},
+			noRowsOverlayComponentParams: {
+			noRowsMessageFunc: () =>
+				"BJ를 후원하여 유저 랭킹에 들어보아요!"
+			}
+		};
+	}
+}
+
+class BalloonTable extends SupportTable {
 	constructor() {
 		super();
 		this.target = document.getElementById('balloonTable');
-		this.gridOptions = {
-			// each entry here represents one column
-			columnDefs: [
-				{
-					lockPosition: 'left',
-					valueGetter: 'node.rowIndex+1',
-					cellClass: 'locked-col',
-					minWidth: 40,
-					maxWidth: 40,
-					suppressNavigable: true,
-					sortable: false,
-				},
-				{
-					field: 'broadcastStation',
-					headerName: '',
-					maxWidth: 49,
-					sortable: false,
-					cellRenderer: ({ data: { userId } }) => {
-						return `<img style="width:25px; height:25px; border-radius: 50%;" src="//profile.img.afreecatv.com/LOGO/${userId.substring(
-							0,
-							2
-						)}/${userId}/${userId}.jpg" onclick="window.open('https://bj.afreecatv.com/${userId}')" alt="">`;
-					},
-				},
-				{ field: 'userNickname', headerName: '닉네임', maxWidth: 100 },
-				{ field: 'count', headerName: '선물개수', maxWidth: 90 },
-				{ field: 'sendCount', headerName: '선물횟수', maxWidth: 90 },
-				{
-					field: 'sendPercent',
-					headerName: '선물개수비율',
-					cellRenderer: 'agSparklineCellRenderer',
-					cellRendererParams: {
-						sparklineOptions: {
-							type: 'bar',
-							fill: '#5470c6',
-							stroke: '#91cc75',
-							highlightStyle: {
-								fill: '#fac858',
-							},
-							valueAxisDomain: [0, 1],
-							paddingOuter: 0,
-							padding: {
-								top: 0,
-								bottom: 0,
-							},
-							axis: {
-								strokeWidth: 0,
-							},
-						},
-					},
-					maxWidth: 115,
-				},
-			],
-
-			// default col def properties get applied to all columns
-			defaultColDef: { sortable: true, resizable: true },
-			paginationPageSize: 10,
-			// rowSelection: 'multiple', // al paginationPageSize: 10, low rows to be selected
-			// animateRows: true, // have rows animate to new positions when sorted
-			// onCellClicked: (params) => {
-			// 	console.log('cell was clicked', params);
-			// },
-		};
 	}
 }
 
-class AdballoonTable extends Table {
+class AdballoonTable extends SupportTable {
 	constructor() {
 		super();
 		this.target = document.getElementById('adballoonTable');
-		this.gridOptions = {
-			// each entry here represents one column
-			columnDefs: [
-				{
-					lockPosition: 'left',
-					valueGetter: 'node.rowIndex+1',
-					cellClass: 'locked-col',
-					minWidth: 40,
-					maxWidth: 40,
-					suppressNavigable: true,
-					sortable: false,
-				},
-				{
-					field: 'broadcastStation',
-					headerName: '',
-					maxWidth: 49,
-					sortable: false,
-					cellRenderer: ({ data: { userId } }) => {
-						return `<img style="width:25px; height:25px; border-radius: 50%;" src="//profile.img.afreecatv.com/LOGO/${userId.substring(
-							0,
-							2
-						)}/${userId}/${userId}.jpg" onclick="window.open('https://bj.afreecatv.com/${userId}')" alt="">`;
-					},
-				},
-				{ field: 'userNickname', headerName: '닉네임', maxWidth: 100 },
-				{ field: 'count', headerName: '선물개수', maxWidth: 90 },
-				{ field: 'sendCount', headerName: '선물횟수', maxWidth: 90 },
-				{
-					field: 'sendPercent',
-					headerName: '선물개수비율',
-					cellRenderer: 'agSparklineCellRenderer',
-					cellRendererParams: {
-						sparklineOptions: {
-							type: 'bar',
-							fill: '#5470c6',
-							stroke: '#91cc75',
-							highlightStyle: {
-								fill: '#fac858',
-							},
-							valueAxisDomain: [0, 1],
-							paddingOuter: 0,
-							padding: {
-								top: 0,
-								bottom: 0,
-							},
-							axis: {
-								strokeWidth: 0,
-							},
-						},
-					},
-					maxWidth: 115,
-				},
-			],
-
-			// default col def properties get applied to all columns
-			defaultColDef: { sortable: true, resizable: true },
-			paginationPageSize: 10,
-		};
 	}
 }
 
-class BattleMissionTable extends Table {
+class BattleMissionTable extends SupportTable {
 	constructor() {
 		super();
 		this.target = document.getElementById('battleMissionTable');
-		this.gridOptions = {
-			// each entry here represents one column
-			columnDefs: [
-				{
-					lockPosition: 'left',
-					valueGetter: 'node.rowIndex+1',
-					cellClass: 'locked-col',
-					minWidth: 40,
-					maxWidth: 40,
-					suppressNavigable: true,
-					sortable: false,
-				},
-				{
-					field: 'broadcastStation',
-					headerName: '',
-					maxWidth: 49,
-					sortable: false,
-					cellRenderer: ({ data: { userId } }) => {
-						return `<img style="width:25px; height:25px; border-radius: 50%;" src="//profile.img.afreecatv.com/LOGO/${userId.substring(
-							0,
-							2
-						)}/${userId}/${userId}.jpg" onclick="window.open('https://bj.afreecatv.com/${userId}')" alt="">`;
-					},
-				},
-				{ field: 'userNickname', headerName: '닉네임', maxWidth: 100 },
-				{ field: 'count', headerName: '선물개수', maxWidth: 90 },
-				{ field: 'sendCount', headerName: '선물횟수', maxWidth: 90 },
-				{
-					field: 'sendPercent',
-					headerName: '선물개수비율',
-					cellRenderer: 'agSparklineCellRenderer',
-					cellRendererParams: {
-						sparklineOptions: {
-							type: 'bar',
-							fill: '#5470c6',
-							stroke: '#91cc75',
-							highlightStyle: {
-								fill: '#fac858',
-							},
-							valueAxisDomain: [0, 1],
-							paddingOuter: 0,
-							padding: {
-								top: 0,
-								bottom: 0,
-							},
-							axis: {
-								strokeWidth: 0,
-							},
-						},
-					},
-					maxWidth: 115,
-				},
-			],
-
-			// default col def properties get applied to all columns
-			defaultColDef: { sortable: true, resizable: true },
-			paginationPageSize: 10,
-		};
 	}
 }
 
-class ChallengeMissionTable extends Table {
+class ChallengeMissionTable extends SupportTable {
 	constructor() {
 		super();
 		this.target = document.getElementById('challengeMissionTable');
-		this.gridOptions = {
-			// each entry here represents one column
-			columnDefs: [
-				{
-					lockPosition: 'left',
-					valueGetter: 'node.rowIndex+1',
-					cellClass: 'locked-col',
-					minWidth: 40,
-					maxWidth: 40,
-					suppressNavigable: true,
-					sortable: false,
-				},
-				{
-					field: 'broadcastStation',
-					headerName: '',
-					maxWidth: 49,
-					sortable: false,
-					cellRenderer: ({ data: { userId } }) => {
-						return `<img style="width:25px; height:25px; border-radius: 50%;" src="//profile.img.afreecatv.com/LOGO/${userId.substring(
-							0,
-							2
-						)}/${userId}/${userId}.jpg" onclick="window.open('https://bj.afreecatv.com/${userId}')" alt="">`;
-					},
-				},
-				{ field: 'userNickname', headerName: '닉네임', maxWidth: 100 },
-				{ field: 'count', headerName: '선물개수', maxWidth: 90 },
-				{ field: 'sendCount', headerName: '선물횟수', maxWidth: 90 },
-				{
-					field: 'sendPercent',
-					headerName: '선물개수비율',
-					cellRenderer: 'agSparklineCellRenderer',
-					cellRendererParams: {
-						sparklineOptions: {
-							type: 'bar',
-							fill: '#5470c6',
-							stroke: '#91cc75',
-							highlightStyle: {
-								fill: '#fac858',
-							},
-							valueAxisDomain: [0, 1],
-							paddingOuter: 0,
-							padding: {
-								top: 0,
-								bottom: 0,
-							},
-							axis: {
-								strokeWidth: 0,
-							},
-						},
-					},
-					maxWidth: 115,
-				},
-			],
-
-			// default col def properties get applied to all columns
-			defaultColDef: { sortable: true, resizable: true },
-			paginationPageSize: 10,
-		};
 	}
 }
